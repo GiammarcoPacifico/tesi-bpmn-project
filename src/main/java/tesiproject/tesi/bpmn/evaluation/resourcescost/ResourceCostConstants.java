b69ed4ad7b31dc986d7ba21781c330fbe5f5b4b5
GiammarcoPacifico <pacificogiammarco@gmail.com>
2026-02-06 17:29:22 +0100

    Sistemata la costante per passare da CO2 a kWh

--- src/main/java/tesiproject/tesi/bpmn/evaluation/resourcescost/ResourceCostConstants.java
+++ src/main/java/tesiproject/tesi/bpmn/evaluation/resourcescost/ResourceCostConstants.java
@@ -7,7 +7,7 @@ public final class ResourceCostConstants {
     private ResourceCostConstants() {}
 
     /* kWh = CO2 * 0.00385 */
-    public static final BigDecimal KWH_PER_G_CO2 = new BigDecimal("0.00385");
+    public static final BigDecimal KWH_PER_G_CO2 = new BigDecimal("0.00333"); //Questa valutazione vale in italia
 
     /* costo di 1 kWh in EUR */
     public static final BigDecimal COST_EUR_PER_KWH = new BigDecimal("0.12762");
